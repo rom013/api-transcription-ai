0cda97ccfa5a41cfa6eb97a809044dec3e6a6e92
rom013 <romullomelo0130@gmail.com>
2024-01-30 18:53:06 -0300

    fet: create route get all prompts

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,10 @@
 import { fastify } from "fastify"
+import { getAllPromptsRoute } from "./routes/get-all-prompts"
 
 const app = fastify()
 
-app.get("/",()=>{
-    return "Hello world"
-})
+/* Routes */
+app.register(getAllPromptsRoute)
 
+/* Start server in port 3333 */
 app.listen({ port: 3333 }).then(()=>console.log("server start"))
