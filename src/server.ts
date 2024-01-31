6f61f618603755d5198a3b84c689a6d1ddd1c89d
rom013 <romullomelo0130@gmail.com>
2024-01-31 16:25:58 -0300

    feat: save transcription in database

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,14 @@
 import { fastify } from "fastify"
 import { getAllPromptsRoute } from "./routes/get-all-prompts"
 import { uploudVideoRoute } from "./routes/uploud-video"
+import { createTranscriptionRoute } from "./routes/create-transcription"
 
 const app = fastify()
 
 /* Routes */
 app.register(getAllPromptsRoute)
 app.register(uploudVideoRoute)
+app.register(createTranscriptionRoute)
 
 /* Start server in port 3333 */
 app.listen({ port: 3333 }).then(()=>console.log("server start"))
